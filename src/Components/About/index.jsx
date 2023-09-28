47a8ec87db89f5a81d61fa7e0eed18e04085d3f2
DawitSishu <dawitalemayhu55@gmail.com>
2023-09-28 03:24:55 -0400

    about us page done

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { DefaultPlayer as Video } from "react-html5video";
-import "react-html5video/dist/styles.css";
+import React, { useEffect, useState, useRef } from "react";
 import { Dialog, IconButton, Typography } from "@mui/material";
-import {
-  Close as CloseIcon,
-  PlayArrow as PlayArrowIcon,
-} from "@mui/icons-material";
 import "./style.css";
-
-import poster from "../../assets/pics/eb4.jpg";
-import vid from "../../assets/promo.mp4";
-
-import mainImg from "../../assets/pics/main.jpg";
+import { gsap } from "gsap";
+import img1 from "../../assets/pics/main.jpg";
+import img2 from "../../assets/pics/main.jpg";
+import img3 from "../../assets/pics/main.jpg";
 import hotelmain from "../../assets/pics/about2.jpg";
 import { Grid } from "@mui/material";
-import thumbnail from "../../assets/pics/tum.jpg";
-import { FaTimes, FaPlay } from "react-icons/fa";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import { FaMapMarkerAlt } from "react-icons/fa";
@@ -24,6 +15,51 @@ function index() {
   const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     window.scrollTo(0, 0);
+  }, []);
+  const tl = useRef();
+  useEffect(() => {
+    tl.current = gsap.timeline({});
+    tl.current.fromTo(
+      ".about-img",
+      {
+        opacity: 0,
+        x: 200,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        stagger: 0.1,
+        ease: "elastic",
+        duration: 3,
+      }
+    );
+
+    tl.current.fromTo(
+      ".logo",
+      {
+        opacity: 0,
+        x: "-100vh",
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 3,
+      },
+      0
+    );
+    tl.current.fromTo(
+      ".about-content-text",
+      {
+        opacity: 0,
+        x: "200vh",
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 3,
+      },
+      0
+    );
   }, []);
 
   const handleOpenPopup = () => {
@@ -54,89 +90,111 @@ function index() {
             variant="h2"
             align="center"
             sx={{ fontWeight: "bold" }}
-            color="#FF00FF"
+            color="Primary"
           >
             About US
           </Typography>
         </Grid>
       </Grid>
-
-      <Grid container spacing={2} justifyContent="center" p={4}>
+      <div className="images">
+        <div className="main-title">
+          <h1>Welcome</h1>
+          <h1>To</h1>
+          <h1>Our Hotel</h1>
+        </div>
+        <div className="container">
+          <input type="radio" name="slider" id="item-1" defaultChecked />
+          <input type="radio" name="slider" id="item-2" />
+          <input type="radio" name="slider" id="item-3" />
+          <div className="cards">
+            <label className="card" htmlFor="item-1" id="song-1">
+              <img src={img2} alt="song" className="about-img" loading="lazy" />
+            </label>
+            <label className="card" htmlFor="item-2" id="song-2">
+              <img src={img1} alt="song" className="about-img" loading="lazy" />
+            </label>
+            <label className="card" htmlFor="item-3" id="song-3">
+              <img src={img3} alt="song" className="about-img" loading="lazy" />
+            </label>
+          </div>
+        </div>
+      </div>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className="logo"
+        >
+          <img
+            src={hotelmain}
+            alt="logp"
+            style={{ width: "75%", height: "80%" }}
+            loading="lazy"
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Grid
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+              padding: 5,
+            }}
+            className="about-content-text"
+          >
+            <Typography
+              variant="h3"
+              align="center"
+              className="about-text-title"
+            >
+              White House Addis Hotel
+            </Typography>
+            <Typography variant="body1" align="center">
+              Welcome to White House Addis Hotel, where luxury, hospitality, and
+              Ethiopian heritage blend seamlessly. Located in the heart of Addis
+              Ababa, our hotel offers personalized service, elegant
+              accommodations, and a perfect balance of modern comforts and
+              traditional aesthetics. Immerse yourself in the vibrant energy of
+              the city, with convenient access to business districts, cultural
+              landmarks, and lively nightlife. Whether for business or leisure,
+              our dedicated concierge team is ready to ensure a seamless and
+              enriching stay. Experience the essence of Ethiopian hospitality at
+              White House Addis Hotel, where every moment is crafted to exceed
+              your expectations.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+      {/* <Grid container spacing={2} justifyContent="center" p={4}>
         <Typography
           align="center"
           variant="h5"
           sx={{ fontWeight: "bold", textDecoration: "blink" }}
         >
-          Welcome to MM Hotel, your gateway to the vibrant city of Dire Dawa
+          Welcome to White House Addis Hotel
         </Typography>
         <br />
         <br />
         <Typography variant="body1" align="center">
-          At MM Hotel, we offer a range of 60 guest rooms, from luxurious Suites
-          to stylish and comfortable deluxe bedrooms, many of which offer
-          magnificent views of the Dire Dawa Mountains. Our prime location in
-          Sabian, just a short distance from the airport, makes MM Hotel Dire
-          Dawa an ideal choice for your stay. During your stay, you can enjoy
-          the convenience of our on-site bar and restaurant, where you can savor
-          delicious meals. We also provide free private parking on site for your
-          convenience. Stay connected with complimentary Wi-Fi available
-          throughout the property. Our guest rooms at MM Hotel are designed with
-          your comfort in mind, featuring air-conditioning, flat-screen TVs with
-          satellite channels, tea-making facilities, and Jacuzzis available in
-          our Suites. Each room comes with a private bathroom equipped with a
-          bath and shower, along with bathrobes and slippers for your comfort.
-          At MM Hotel Dire Dawa, we go the extra mile to enhance your stay with
-          a free shuttle service from the airport, a 24-hour front desk, and
-          meeting facilities. Additionally, our shared lounge provides a cozy
-          space for guests to relax and watch TV. We look forward to hosting you
-          and making your stay in Dire Dawa truly memorable.
+          Welcome to White House Addis Hotel, where luxury, hospitality, and
+          Ethiopian heritage blend seamlessly. Located in the heart of Addis
+          Ababa, our hotel offers personalized service, elegant accommodations,
+          and a perfect balance of modern comforts and traditional aesthetics.
+          Immerse yourself in the vibrant energy of the city, with convenient
+          access to business districts, cultural landmarks, and lively
+          nightlife. Whether for business or leisure, our dedicated concierge
+          team is ready to ensure a seamless and enriching stay. Experience the
+          essence of Ethiopian hospitality at White House Addis Hotel, where
+          every moment is crafted to exceed your expectations.
         </Typography>
-      </Grid>
-      <Typography variant="h3" align="center">
-        Your <FaMapMarkerAlt color="#FF00FF" /> Gateway to{" "}
-        <FaMapMarkerAlt color="#FF00FF" /> Dire Dawa
-      </Typography>
-      <div>
-        {!isOpen ? (
-          <div className="play-image">
-            <img src={poster} alt="Play Video" className="poster-image" />
-            <div className="play-button-overlay" onClick={handleOpenPopup}>
-              <PlayArrowIcon
-                sx={{ fontSize: "100px", color: "#FF00FF" }}
-                className="play-button-icon"
-              />
-            </div>
-          </div>
-        ) : null}
-        <Dialog
-          open={isOpen}
-          onClose={handleClosePopup}
-          fullWidth
-          maxWidth="lg"
-          sx={{ height: "100vh" }}
-        >
-          <div className="popup-overlay">
-            <IconButton
-              edge="end"
-              color="inherit"
-              onClick={handleClosePopup}
-              className="close-button"
-              sx={{ position: "fixed", top: "10px", left: "10px" }}
-            >
-              <CloseIcon sx={{ fontSize: "40px" }} />
-            </IconButton>
-            <div className="video-container">
-              <Video
-                autoPlay
-                loop
-                controls={["PlayPause", "Seek", "Time", "Volume", "Fullscreen"]}
-              >
-                <source src={vid} type="video/mp4" />
-              </Video>
-            </div>
-          </div>
-        </Dialog>
-      </div>
+      </Grid> */}
       <Footer />
     </div>
   );
