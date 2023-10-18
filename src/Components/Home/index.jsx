31fe68671e6385a597446eed1787ce5beab04a36
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-18 13:14:35 -0400

    almost done

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,11 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import { Grid, Typography, Divider, Box, Button } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  Divider,
+  Box,
+  Button,
+  ListItem,
+  ListItemIcon,
+} from "@mui/material";
 import { FaMapMarkerAlt, FaStar } from "react-icons/fa";
 import "./style.css";
 import { IoIosArrowForward } from "react-icons/io";
 import { BiRestaurant } from "react-icons/bi";
+import RoomServiceIcon from "@mui/icons-material/RoomService";
+import WifiIcon from "@mui/icons-material/Wifi";
+import LocalParkingIcon from "@mui/icons-material/LocalParking";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { MdRoomService, MdLocalLaundryService } from "react-icons/md";
 import {
   FaDumbbell,
@@ -21,14 +33,17 @@ import img1 from "../../assets/white2.jpg";
 import Carou from "./Carousel";
 
 const facilities = [
-  { name: "Restaurant", icon: <BiRestaurant size={33} /> },
-  { name: "Room Service", icon: <MdRoomService size={33} /> },
-  { name: "Laundary", icon: <MdLocalLaundryService size={33} /> },
-  { name: "Bar", icon: <FaGlassMartini size={33} /> },
-  { name: "Gym", icon: <FaDumbbell size={33} /> },
-  { name: "Spa", icon: <FaSpa size={33} /> },
-  { name: "Wifi", icon: <FaWifi size={33} /> },
-  { name: "24/7 Front Desk", icon: <FaClock size={33} /> },
+  { name: "Restaurant", icon: <BiRestaurant size={33} color="#FFEE49" /> },
+  { name: "Room Service", icon: <MdRoomService size={33} color="#FFEE49" /> },
+  {
+    name: "Laundary",
+    icon: <MdLocalLaundryService size={33} color="#FFEE49" />,
+  },
+  { name: "Bar", icon: <FaGlassMartini size={33} color="#FFEE49" /> },
+  { name: "Gym", icon: <FaDumbbell size={33} color="#FFEE49" /> },
+  { name: "Spa", icon: <FaSpa size={33} color="#FFEE49" /> },
+  { name: "Wifi", icon: <FaWifi size={33} color="#FFEE49" /> },
+  { name: "24/7 Front Desk", icon: <FaClock size={33} color="#FFEE49" /> },
 ];
 
 const index = () => {
@@ -201,6 +216,67 @@ const index = () => {
           <Link to="/gallery">
             <Button variant="contained">Ecplore More</Button>
           </Link>
+        </Grid>
+      </Grid>
+      <Grid container spacing={2} sx={{ padding: "20px", margin: 1 }}>
+        <Grid item xs={12}>
+          <Typography
+            variant="h4"
+            align="center"
+            color="primary"
+            sx={{ marginBottom: "20px", fontWeight: "bold" }}
+          >
+            Where You Can Find Us
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          zIndex={1}
+          justifyContent="center"
+          sx={{ height: "60vh", paddingRight: 2 }}
+        >
+          <Box sx={{ width: "100%", height: "100%" }} className="home-map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.7738841989017!2d38.79160637379982!3d8.99294269106697!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b84e3cf649285%3A0x84bd06c4e5105ff9!2zV2hpdGUgSG91c2UgQWRkaXMgSG90ZWwgfCBCb2xlIHwg4YuL4Yut4Ym1IOGIg-GLjeGItSDhiqDhi7LhiLUgfCDhiabhiIwgfA!5e0!3m2!1sen!2set!4v1695880148030!5m2!1sen!2set"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12} sm={6} sx={{ paddingLeft: 2 }}>
+          <ListItem>
+            <ListItemIcon>
+              <WifiIcon color="primary" />
+            </ListItemIcon>
+            <Typography>Free Wi-Fi Internet in Rooms</Typography>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <RoomServiceIcon color="primary" />
+            </ListItemIcon>
+            <Typography>
+              In Room Dining (Room Service) Available At Anytime
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <LocalParkingIcon color="primary" />
+            </ListItemIcon>
+            <Typography>Local Self Parking: For Free</Typography>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <LocationOnIcon color="primary" />
+            </ListItemIcon>
+            <Typography>Bole sub city, woreda 03</Typography>
+          </ListItem>
         </Grid>
       </Grid>
       {/* <Grid
